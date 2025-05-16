10e1521e3ac1d3d9fde4a4b5f4a012b69bc3629e
Santos Enoque <santosenoque.ss@gmail.com>
2025-05-16 22:24:01 +0900

    vgv

---
+++ flutter/vgv/android/app/src/main/kotlin/com/custom/org/vgv/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.custom.org.vgv
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter/vgv/lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:vgv/app/app.dart';
+import 'package:vgv/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
