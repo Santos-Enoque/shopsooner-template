0a1246d916468a061894d17d4aff31f3f3f56387
Santos Enoque <santosenoque.ss@gmail.com>
2025-05-18 04:05:04 +0900

    add main

---
+++ flutter/vgv/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/widgets.dart';
+import 'package:vgv/app.dart';
+import 'package:vgv/config/bootstrap.dart';
+import 'package:vgv/config/env_loader.dart';
+import 'package:vgv/dependency_injection.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final config = EnvLoader.getConfig('development');
+
+  await configureDependencies(config: config);
+
+  await bootstrap(
+    () => const App(),
+    config: config,
+  );
+}
